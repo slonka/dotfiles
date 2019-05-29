2f5d6532bbb54974370e555018aba4517351cd34
Krzysztof Slonka <slonka@users.noreply.github.com>
2019-05-29 14:59:30 +0200

    Add docker ips funtion

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,1 +1,6 @@
 grep1() { awk -v pattern="${1:?pattern is empty}" 'NR==1 || $0~pattern' "${2:?filename is empty}"; }
+
+function drips(){
+    docker ps -q | xargs -n 1 docker inspect --format '{{ .NetworkSettings.IPAddress }} {{ .Name }}' | sed 's/ \// /'
+}
+
