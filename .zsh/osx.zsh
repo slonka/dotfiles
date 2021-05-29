b48f21734ae0df646ea707f141ed538df59d622b
slonka <slonka@users.noreply.github.com>
2021-05-29 19:27:44 +0200

    Add osx key remapping

---
+++ .zsh/osx.zsh
@@ -0,0 +1,6 @@
+hidutil property --set '{"UserKeyMapping":
+    [{"HIDKeyboardModifierMappingSrc":0x7000000e7,
+      "HIDKeyboardModifierMappingDst":0x7000000e6},
+     {"HIDKeyboardModifierMappingSrc":0x7000000e6,
+      "HIDKeyboardModifierMappingDst":0x7000000e7}]
+}'
