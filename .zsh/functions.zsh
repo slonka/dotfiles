37e6bebf4e9a746960840d619ce0f982c21b22d2
Krzysztof Słonka <slonka@users.noreply.github.com>
2025-01-16 20:59:51 +0100

    Update functions.zsh

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -4,3 +4,18 @@ function docker_ips(){
 	docker ps -q | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Config.Hostname }} {{ .Config.Image }}' | sed 's/ \// /'
 }
 
+function git_diff_lines(){
+	git diff | gawk '
+	  match($0,"^@@ -([0-9]+),([0-9]+) [+]([0-9]+),([0-9]+) @@",a){
+	    left=a[1]
+	    ll=length(a[2])
+	    right=a[3]
+	    rl=length(a[4])
+	  }
+	  /^(---|\+\+\+|[^-+ ])/{ print;next }
+	  { line=substr($0,2) }
+	  /^[-]/{ printf "-%"ll"s %"rl"s:%s\n",left++,""     ,line;next }
+	  /^[+]/{ printf "+%"ll"s %"rl"s:%s\n",""    ,right++,line;next }
+	        { printf " %"ll"s %"rl"s:%s\n",left++,right++,line }
+	'
+}
